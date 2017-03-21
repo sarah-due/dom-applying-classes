004f626c491e2461e1207ac0637291ad9e7d37f7
Sarah Dueweke <sdueweke@gmail.com>
2017-03-21 13:30:28 +1300

    Add function 'three' and styling

--- classes.js
+++ classes.js
@@ -4,26 +4,24 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
+
   one()
   two()
-  // Your turn! Create a new function called `two`, then call it from here.
+  three()
+
 }
 
 function one () {
-  // First, we have to find the element:
   var one = document.getElementById('one')
-
-  // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
 function two () {
   var two = document.getElementById('two')
   two.classList.add('green')
 }
 
-// CREATE FUNCTION three HERE
-
-// CREATE FUNCTION makeVisible HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('magenta')
+}
